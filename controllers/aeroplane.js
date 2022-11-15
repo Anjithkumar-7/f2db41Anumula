e9cb4831ee90707c1a6bece6d513e2d03c52cfb6
Anjithkumar-7 <anjithsuma@gmail.com>
2022-11-14 21:58:19 -0600

    Adding the required files for the main Assignment.

--- controllers/aeroplane.js
+++ controllers/aeroplane.js
@@ -1,5 +1,5 @@
 var aeroplane = require('../models/aeroplane');
-// List of all Costumes
+// List of all Aeroplanes
 exports.aeroplane_list = async function(req, res) {
     try{
     theaeroplane = await aeroplane.find();
@@ -10,19 +10,18 @@ exports.aeroplane_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
-// for a specific Aeroplane.
-exports.aeroplane_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Aeroplane detail: ' + req.params.id);
-};
-// Handle Aeroplane delete form on DELETE.
-exports.aeroplane_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Aeroplane delete DELETE ' + req.params.id);
-};
-// Handle Aeroplane update form on PUT.
-exports.aeroplane_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Aeroplane update PUT' + req.params.id);
-};
-
+// Handle Aeroplane delete on DELETE. 
+exports.aeroplane_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await aeroplane.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
 // VIEWS
 // Handle a show all view
 exports.aeroplane_view_all_Page = async function(req, res) {
@@ -36,14 +35,14 @@ exports.aeroplane_view_all_Page = async function(req, res) {
     }
    };
 
-   // Handle Costume create on POST.
+   // Handle Aeroplane create on POST.
 exports.aeroplane_create_post = async function(req, res) {
     console.log(req.body)
     let document = new aeroplane();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"aeroplane_type":"goat", "cost":12, "size":"large"}
     document.aer_class = req.body.aer_class;
     document.aer_fare = req.body.aer_fare;
     document.aer_mem = req.body.aer_mem;
@@ -56,3 +55,92 @@ exports.aeroplane_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // for a specific aeroplane. 
+exports.aeroplane_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await aeroplane.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};
+
+//Handle aeroplane update form on PUT. 
+exports.aeroplane_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await aeroplane.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.aer_class)  
+               toUpdate.aer_class = req.body.aer_class; 
+        if(req.body.aer_fare) toUpdate.aer_fare = req.body.aer_fare; 
+        if(req.body.aer_mem) toUpdate.aer_mem = req.body.aer_mem; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+
+//Handle a show one view with id specified by query 
+exports.aeroplane_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await aeroplane.findById( req.query.id) 
+        res.render('aeroplanedetail',  
+{ title: 'Aeroplane Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for creating a aeroplane. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.aeroplane_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('aeroplanecreate', { title: 'Aeroplane Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a aeroplane. 
+// query provides the id 
+exports.aeroplane_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await aeroplane.findById(req.query.id) 
+        res.render('aeroplaneupdate', { title: 'Aeroplane Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.aeroplane_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await aeroplane.findById(req.query.id) 
+        res.render('aeroplanedelete', { title: 'Aeroplane Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/aeroplane.js
+++ routes/aeroplane.js
@@ -4,4 +4,9 @@ var router = express.Router();
 
 /* GET aeroplane */
 router.get('/', aeroplane_controlers.aeroplane_view_all_Page );
+router.get('/detail', aeroplane_controlers.aeroplane_view_one_Page); 
+router.get('/create', aeroplane_controlers.aeroplane_create_Page); 
+router.get('/update', aeroplane_controlers.aeroplane_update_Page);
+router.get('/delete', aeroplane_controlers.aeroplane_delete_Page); 
 module.exports = router;
+
